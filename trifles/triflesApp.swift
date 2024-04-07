0a3bbbde95634557360b8cd5b12966a8df1d12cd
Thomas Wang <wang.thomasdavid@gmail.com>
2024-04-07 11:10:07 -0400

    Initial Commit

---
+++ trifles/triflesApp.swift
@@ -0,0 +1,17 @@
+//
+//  triflesApp.swift
+//  trifles
+//
+//  Created by Thomas Wang on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct triflesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
